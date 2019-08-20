5af77561e7da8f052d30cca9f90d821d635ac328
Chakri Lowphansirikul <arta@Chakris-MacBook-Pro-2.local>
2019-08-20 16:06:28 +0700

    set <a> target as _blank (open new tab)

--- source/conf.py
+++ source/conf.py
@@ -83,8 +83,8 @@ nbsphinx_prolog = r"""
 
     .. nbinfo::
         Interactive online version: 
-        :raw-html:`<a href="https://mybinder.org/v2/gh/pythainlp/tutorials/master?filepath=source/notebooks/{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`
-        :raw-html:`<a href="https://colab.research.google.com/github/PyThaiNLP/tutorials/blob/master/source/notebooks/{{ docname }}"><img alt="Google Colab badge" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<a target="_blank" href="https://mybinder.org/v2/gh/pythainlp/tutorials/master?filepath=source/notebooks/{{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<a target="_blank" href="https://colab.research.google.com/github/PyThaiNLP/tutorials/blob/master/source/notebooks/{{ docname }}"><img alt="Google Colab badge" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>`
 .. raw:: latex
 
     \nbsphinxstartnotebook{The following section was created from
